9f2d1768169412cb4a339b3e8e746198de5ac568
Manish KC <kcmanish89@gmail.com>
2019-07-11 09:29:22 -0700

    added schema and update env file

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
 
+
+//Global vars
 const PORT = process.env.PORT || 3001;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', error=> {
+  console.error(error);
+})
 
 //Apps
 const app = express();
